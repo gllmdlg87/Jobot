03c3d275d21c4e09c4127ffac78b4b918657784c
gllmdlg87 <88460336+gllmdlg87@users.noreply.github.com>
2023-04-23 20:23:37 +0000

    added chatbox

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { ReactMarkdown } from "react-markdown";
+
+const SYSTEM_MESSAGE = "You are Jobot, an helpful AI created by Jovian using state-of-the art ML Models";
 
 export default function Home() {
-  const [apiKey, setApiKey] = useState('');
+  const [apiKey, setApiKey] = useState("");
+  
+  const [messages, setMessages] = useState([
+    { role: "system", content: SYSTEM_MESSAGE},
+  ]);
+
+  const [userMessage, setUserMessage] = useState("");
   
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
   async function sendRequest() {
+    // update the message history
+    const newMessage = {role: "user", content: userMessage};
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+    setUserMessage("");
+
     const response = await fetch(API_URL, {
       method: "POST",
       headers: {
@@ -14,16 +30,22 @@ export default function Home() {
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        messages: [{role:"user", content: "Hello!"}],
+        messages: newMessages,
       }),
     });
 
     const responseJson = await response.json();
-    
-    console.log("responseJson", responseJson);
+
+    const newBotMessage = responseJson.choices[0].message;
+
+    const newMessage2 = [...newMessages, newBotMessage];
+
+    setMessages(newMessage2);
+  
   }
 
   return <div className="flex flex-col h-screen">
+    { /* Navigation Bar */}
     <nav className = "shadow px-4 py-2 flex flex-rox justify-between items-center">
         <div className="text-xl font-bold">Jobot</div>
         <div>
@@ -36,12 +58,36 @@ export default function Home() {
         </div>
     </nav>
 
-    <div className="p-4">
-      <button 
-      className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white"
-      onClick={sendRequest} >
-        Send request
-      </button>
+    {/* Message History */}
+    <div className="flex-1 overflow-y-scroll">
+      <div className="w-full max-w-screen-md mx-auto px-4">
+        {messages
+          .filter((message) => message.role !== "system")
+          .map((message, idx) => (
+          <div key={idx} className='mt-3'>
+            <div className="font-bold">
+              {message.role === "user" ? "You" : "Jobo"}
+            </div>
+            <div className="text-lg prose">
+              <ReactMarkdown>{message.content}</ReactMarkdown>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    {/* Message Input Box */}
+    <div>
+      <div className="w-full max-w-screen mx-auto flex px-4 pb-4">
+        <textarea 
+        value={userMessage}
+        onChange={(e) => setUserMessage(e.target.value)}
+        className="border text-lg rounded-md p-1 flex-1">
+        </textarea>
+        <button className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-2 ml-2">
+          Send
+        </button>
+      </div>
     </div>
 
   </div>
